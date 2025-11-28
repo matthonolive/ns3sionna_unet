5a356752eab46d88fd07b1b5d744a5c2d3e27237
matthonolive <matthewhdmd@gmail.com>
2025-11-27 23:01:59 -0500

    misc

--- bash.sh
+++ bash.sh
@@ -1,2 +1,4 @@
 export DRJIT_LIBOPTIX_PATH=$HOME/optix-580.105.08/libnvoptix.so.1
 export LD_LIBRARY_PATH=$HOME/optix-580.105.08:$LD_LIBRARY_PATH
+
+export DRJIT_LIBLLVM_PATH=/usr/lib/llvm-15/lib/libLLVM-15.so 
