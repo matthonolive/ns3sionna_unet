0352e56333be523ec27f84dbabb3e1ebac1b3eca
Matthew Honorio Oliveira <matthewhdmd@gmail.com>
2025-12-11 22:49:01 -0500

    Revert "sionna1.2.1"

--- examples/example-sionna-sensing.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ../../../
-
-echo "Running ns3"
-./ns3 run ns3sionna-example-sionna-sensing
-
-echo "Plotting results"
-python3 ./contrib/sionna/examples/plot_csi.py csi_node0.csv csi_node1.csv
